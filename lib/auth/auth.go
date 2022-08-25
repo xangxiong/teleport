a8845b72cb1f363b54a66d44b11bb1e1ccdc796d
joerger <bjoerger@goteleport.com>
2022-08-25 13:24:22 -0700

    Add private key policy enforcement.
      - Add private key policy cert extensions and enforcement.
    
      - Add private key policy settings and attestation logic.
    
      - Wire attestation request through login endpoints.
    
      - Store attestation data for reissue requests.
    
      - Add private key policy discovery and logic.
    
      - Guess private key policy on initial login.
    
      - Relogin on hardware key policy errors.
    
      - Include integration with Teleport Connect.
    
      - Add Hardware Key licensing.

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -63,6 +63,7 @@ import (
 	apievents "github.com/gravitational/teleport/api/types/events"
 	"github.com/gravitational/teleport/api/types/wrappers"
 	apiutils "github.com/gravitational/teleport/api/utils"
+	"github.com/gravitational/teleport/api/utils/keys"
 	"github.com/gravitational/teleport/api/utils/retryutils"
 	"github.com/gravitational/teleport/lib/auth/keystore"
 	"github.com/gravitational/teleport/lib/auth/native"
@@ -912,6 +913,8 @@ type certRequest struct {
 	// connectionDiagnosticID contains the ID of the ConnectionDiagnostic.
 	// The Node/Agent will append connection traces to this instance.
 	connectionDiagnosticID string
+	// attestationRequest is an attestation request associated with the given public key.
+	attestationRequest *keys.AttestationRequest
 }
 
 // check verifies the cert request is valid.
@@ -1164,6 +1167,12 @@ func (a *Server) generateUserCert(req certRequest) (*proto.Certs, error) {
 		}
 	}
 
+	requiredKeyPolicy := req.checker.PrivateKeyPolicy(authPref.GetPrivateKeyPolicy())
+	privateKeyPolicy, err := modules.GetModules().AttestHardwareKey(ctx, a, requiredKeyPolicy, req.attestationRequest, cryptoPubKey, sessionTTL)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
 	clusterName, err := a.GetDomainName()
 	if err != nil {
 		return nil, trace.Wrap(err)
@@ -1230,6 +1239,7 @@ func (a *Server) generateUserCert(req certRequest) (*proto.Certs, error) {
 		AllowedResourceIDs:     requestedResourcesStr,
 		SourceIP:               req.sourceIP,
 		ConnectionDiagnosticID: req.connectionDiagnosticID,
+		PrivateKeyPolicy:       privateKeyPolicy,
 	}
 	sshCert, err := a.Authority.GenerateUserCert(params)
 	if err != nil {
@@ -1311,6 +1321,7 @@ func (a *Server) generateUserCert(req certRequest) (*proto.Certs, error) {
 		Renewable:          req.renewable,
 		Generation:         req.generation,
 		AllowedResourceIDs: req.checker.GetAllowedResourceIDs(),
+		PrivateKeyPolicy:   privateKeyPolicy,
 	}
 	subject, err := identity.Subject()
 	if err != nil {
