aa7d6e636ff2ecd50b8e63a8f2421ccf0d43b4a7
Reed Loden <reed@goteleport.com>
2022-09-19 15:02:14 -0700

    terraform: Fix all `tfsec`, `tflint`, and `ansible-lint` warnings within `examples/aws/terraform`
    There were numerous `tfsec`, `tflint`, and `ansible-lint` warnings within the
    `examples/aws/terraform` directory. Address all issues, adding ignores
    where needed to bring warning count down to zero.

--- examples/aws/terraform/starter-cluster/data.tf
+++ examples/aws/terraform/starter-cluster/data.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
@@ -15,8 +15,11 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "all" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "all" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 data "aws_ami" "base" {
