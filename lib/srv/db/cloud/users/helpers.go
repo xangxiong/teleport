979252b8e144ace210b62b7e338c3bd03ae59884
dl-synesis <dl-synesis-ops@arris.com>
2022-09-26 17:48:34 +0000

    slim more db

--- lib/srv/db/cloud/users/helpers.go
+++ lib/srv/db/cloud/users/helpers.go
@@ -16,153 +16,153 @@ limitations under the License.
 
 package users
 
-import (
-	"strings"
-	"sync"
+// import (
+// 	"strings"
+// 	"sync"
 
-	"github.com/aws/aws-sdk-go/aws/arn"
-	"github.com/gravitational/trace"
+// 	"github.com/aws/aws-sdk-go/aws/arn"
+// 	"github.com/gravitational/trace"
 
-	"github.com/gravitational/teleport/api/types"
-	"github.com/gravitational/teleport/lib/srv/db/common"
-	"github.com/gravitational/teleport/lib/srv/db/secrets"
-	"github.com/gravitational/teleport/lib/utils"
-)
+// 	"github.com/gravitational/teleport/api/types"
+// 	"github.com/gravitational/teleport/lib/srv/db/common"
+// 	"github.com/gravitational/teleport/lib/srv/db/secrets"
+// 	"github.com/gravitational/teleport/lib/utils"
+// )
 
-// lookupMap is a mapping of database objects to their managed users.
-type lookupMap struct {
-	byDatabase map[types.Database][]User
-	mu         sync.RWMutex
-}
+// // lookupMap is a mapping of database objects to their managed users.
+// type lookupMap struct {
+// 	byDatabase map[types.Database][]User
+// 	mu         sync.RWMutex
+// }
 
-// newLookupMap creates a new lookup map.
-func newLookupMap() *lookupMap {
-	return &lookupMap{
-		byDatabase: make(map[types.Database][]User),
-	}
-}
+// // newLookupMap creates a new lookup map.
+// func newLookupMap() *lookupMap {
+// 	return &lookupMap{
+// 		byDatabase: make(map[types.Database][]User),
+// 	}
+// }
 
-// getDatabaseUser finds a database user by database username.
-func (m *lookupMap) getDatabaseUser(database types.Database, username string) (User, bool) {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
+// // getDatabaseUser finds a database user by database username.
+// func (m *lookupMap) getDatabaseUser(database types.Database, username string) (User, bool) {
+// 	m.mu.RLock()
+// 	defer m.mu.RUnlock()
 
-	for _, user := range m.byDatabase[database] {
-		if user.GetDatabaseUsername() == username {
-			return user, true
-		}
-	}
-	return nil, false
-}
+// 	for _, user := range m.byDatabase[database] {
+// 		if user.GetDatabaseUsername() == username {
+// 			return user, true
+// 		}
+// 	}
+// 	return nil, false
+// }
 
-// setDatabaseUsers sets the database users for future lookups.
-func (m *lookupMap) setDatabaseUsers(database types.Database, users []User) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+// // setDatabaseUsers sets the database users for future lookups.
+// func (m *lookupMap) setDatabaseUsers(database types.Database, users []User) {
+// 	m.mu.Lock()
+// 	defer m.mu.Unlock()
 
-	if len(users) > 0 {
-		m.byDatabase[database] = users
-	} else {
-		delete(m.byDatabase, database)
+// 	if len(users) > 0 {
+// 		m.byDatabase[database] = users
+// 	} else {
+// 		delete(m.byDatabase, database)
 
-		// Short circuit.
-		if len(database.GetManagedUsers()) == 0 {
-			return
-		}
-	}
+// 		// Short circuit.
+// 		if len(database.GetManagedUsers()) == 0 {
+// 			return
+// 		}
+// 	}
 
-	// Update database resource.
-	var usernames []string
-	for _, user := range users {
-		usernames = append(usernames, user.GetDatabaseUsername())
-	}
-	database.SetManagedUsers(usernames)
-}
+// 	// Update database resource.
+// 	var usernames []string
+// 	for _, user := range users {
+// 		usernames = append(usernames, user.GetDatabaseUsername())
+// 	}
+// 	database.SetManagedUsers(usernames)
+// }
 
-// removeUnusedDatabases removes unused databases by comparing with provided
-// active databases.
-func (m *lookupMap) removeUnusedDatabases(activeDatabases types.Databases) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+// // removeUnusedDatabases removes unused databases by comparing with provided
+// // active databases.
+// func (m *lookupMap) removeUnusedDatabases(activeDatabases types.Databases) {
+// 	m.mu.Lock()
+// 	defer m.mu.Unlock()
 
-	for database := range m.byDatabase {
-		if isActive := findDatabase(activeDatabases, database); !isActive {
-			delete(m.byDatabase, database)
-		}
-	}
-}
+// 	for database := range m.byDatabase {
+// 		if isActive := findDatabase(activeDatabases, database); !isActive {
+// 			delete(m.byDatabase, database)
+// 		}
+// 	}
+// }
 
-// findDatabase finds the database object in provided list of databases.
-func findDatabase(databases types.Databases, database types.Database) bool {
-	for i := range databases {
-		if databases[i] == database {
-			return true
-		}
-	}
-	return false
-}
+// // findDatabase finds the database object in provided list of databases.
+// func findDatabase(databases types.Databases, database types.Database) bool {
+// 	for i := range databases {
+// 		if databases[i] == database {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
-// usersByID returns a map of users by their IDs.
-func (m *lookupMap) usersByID() map[string]User {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
+// // usersByID returns a map of users by their IDs.
+// func (m *lookupMap) usersByID() map[string]User {
+// 	m.mu.RLock()
+// 	defer m.mu.RUnlock()
 
-	usersByID := make(map[string]User)
-	for _, users := range m.byDatabase {
-		for _, user := range users {
-			usersByID[user.GetID()] = user
-		}
-	}
-	return usersByID
-}
+// 	usersByID := make(map[string]User)
+// 	for _, users := range m.byDatabase {
+// 		for _, user := range users {
+// 			usersByID[user.GetID()] = user
+// 		}
+// 	}
+// 	return usersByID
+// }
 
-// secretKeyFromAWSARN creates a secret key with provided ARN.
-func secretKeyFromAWSARN(inputARN string) (string, error) {
-	// Example ElastiCache User ARN looks like this:
-	// arn:aws:elasticache:<region>:<account-id>:user:<user-id>
-	//
-	// Make an unique secret key like this:
-	// elasticache/<region>/<account-id>/user/<user-id>
-	parsed, err := arn.Parse(inputARN)
-	if err != nil {
-		return "", trace.BadParameter(err.Error())
-	}
-	return secrets.Key(
-		parsed.Service,
-		parsed.Region,
-		parsed.AccountID,
-		strings.ReplaceAll(parsed.Resource, ":", "/"),
-	), nil
-}
+// // secretKeyFromAWSARN creates a secret key with provided ARN.
+// func secretKeyFromAWSARN(inputARN string) (string, error) {
+// 	// Example ElastiCache User ARN looks like this:
+// 	// arn:aws:elasticache:<region>:<account-id>:user:<user-id>
+// 	//
+// 	// Make an unique secret key like this:
+// 	// elasticache/<region>/<account-id>/user/<user-id>
+// 	parsed, err := arn.Parse(inputARN)
+// 	if err != nil {
+// 		return "", trace.BadParameter(err.Error())
+// 	}
+// 	return secrets.Key(
+// 		parsed.Service,
+// 		parsed.Region,
+// 		parsed.AccountID,
+// 		strings.ReplaceAll(parsed.Resource, ":", "/"),
+// 	), nil
+// }
 
-// genRandomPassword generate a random password with provided length.
-func genRandomPassword(length int) (string, error) {
-	if length <= 0 {
-		return "", trace.BadParameter("invalid random value length")
-	}
+// // genRandomPassword generate a random password with provided length.
+// func genRandomPassword(length int) (string, error) {
+// 	if length <= 0 {
+// 		return "", trace.BadParameter("invalid random value length")
+// 	}
 
-	// Hex generated from CryptoRandomHex is twice of the input.
-	hex, err := utils.CryptoRandomHex((length + 1) / 2)
-	if err != nil {
-		return "", trace.Wrap(err)
-	} else if len(hex) < length {
-		return "", trace.CompareFailed("generated hex is too short")
-	}
-	return hex[:length], nil
-}
+// 	// Hex generated from CryptoRandomHex is twice of the input.
+// 	hex, err := utils.CryptoRandomHex((length + 1) / 2)
+// 	if err != nil {
+// 		return "", trace.Wrap(err)
+// 	} else if len(hex) < length {
+// 		return "", trace.CompareFailed("generated hex is too short")
+// 	}
+// 	return hex[:length], nil
+// }
 
-// newSecretStore create a new secrets store helper for provided database.
-func newSecretStore(database types.Database, clients common.CloudClients) (secrets.Secrets, error) {
-	secretStoreConfig := database.GetSecretStore()
+// // newSecretStore create a new secrets store helper for provided database.
+// func newSecretStore(database types.Database, clients common.CloudClients) (secrets.Secrets, error) {
+// 	secretStoreConfig := database.GetSecretStore()
 
-	client, err := clients.GetAWSSecretsManagerClient(database.GetAWS().Region)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
+// 	client, err := clients.GetAWSSecretsManagerClient(database.GetAWS().Region)
+// 	if err != nil {
+// 		return nil, trace.Wrap(err)
+// 	}
 
-	return secrets.NewAWSSecretsManager(secrets.AWSSecretsManagerConfig{
-		KeyPrefix: secretStoreConfig.KeyPrefix,
-		KMSKeyID:  secretStoreConfig.KMSKeyID,
-		Client:    client,
-	})
-}
+// 	return secrets.NewAWSSecretsManager(secrets.AWSSecretsManagerConfig{
+// 		KeyPrefix: secretStoreConfig.KeyPrefix,
+// 		KMSKeyID:  secretStoreConfig.KMSKeyID,
+// 		Client:    client,
+// 	})
+// }
--- lib/srv/db/cloud/users/user_test.go
+++ lib/srv/db/cloud/users/user_test.go
@@ -16,137 +16,137 @@ limitations under the License.
 
 package users
 
-import (
-	"context"
-	"fmt"
-	"testing"
-	"time"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"testing"
+// 	"time"
 
-	libsecrets "github.com/gravitational/teleport/lib/srv/db/secrets"
+// 	libsecrets "github.com/gravitational/teleport/lib/srv/db/secrets"
 
-	"github.com/gravitational/trace"
-	"github.com/jonboulle/clockwork"
-	"github.com/stretchr/testify/require"
-)
+// 	"github.com/gravitational/trace"
+// 	"github.com/jonboulle/clockwork"
+// 	"github.com/stretchr/testify/require"
+// )
 
-func TestBaseUser(t *testing.T) {
-	t.Parallel()
+// func TestBaseUser(t *testing.T) {
+// 	t.Parallel()
 
-	ctx := context.Background()
-	clock := clockwork.NewFakeClock()
-	mockCloudResource := newMockCloudResource()
+// 	ctx := context.Background()
+// 	clock := clockwork.NewFakeClock()
+// 	mockCloudResource := newMockCloudResource()
 
-	secrets, err := libsecrets.NewAWSSecretsManager(libsecrets.AWSSecretsManagerConfig{
-		Client: libsecrets.NewMockSecretsManagerClient(libsecrets.MockSecretsManagerClientConfig{
-			Clock: clock,
-		}),
-	})
-	require.NoError(t, err)
+// 	secrets, err := libsecrets.NewAWSSecretsManager(libsecrets.AWSSecretsManagerConfig{
+// 		Client: libsecrets.NewMockSecretsManagerClient(libsecrets.MockSecretsManagerClientConfig{
+// 			Clock: clock,
+// 		}),
+// 	})
+// 	require.NoError(t, err)
 
-	user := &baseUser{
-		secrets:                     secrets,
-		secretKey:                   "local/testuser",
-		secretTTL:                   time.Hour,
-		databaseUsername:            "testuser",
-		maxPasswordLength:           10,
-		usePreviousPasswordForLogin: true,
-		clock:                       clock,
-		cloudResource:               mockCloudResource,
-	}
+// 	user := &baseUser{
+// 		secrets:                     secrets,
+// 		secretKey:                   "local/testuser",
+// 		secretTTL:                   time.Hour,
+// 		databaseUsername:            "testuser",
+// 		maxPasswordLength:           10,
+// 		usePreviousPasswordForLogin: true,
+// 		clock:                       clock,
+// 		cloudResource:               mockCloudResource,
+// 	}
 
-	t.Run("CheckAndSetDafaults", func(t *testing.T) {
-		require.NoError(t, user.CheckAndSetDefaults())
-		require.Equal(t, "local/testuser", user.GetID())
-		require.Equal(t, "local/testuser", fmt.Sprintf("%v", user))
-		require.Equal(t, "testuser", user.GetDatabaseUsername())
-	})
+// 	t.Run("CheckAndSetDafaults", func(t *testing.T) {
+// 		require.NoError(t, user.CheckAndSetDefaults())
+// 		require.Equal(t, "local/testuser", user.GetID())
+// 		require.Equal(t, "local/testuser", fmt.Sprintf("%v", user))
+// 		require.Equal(t, "testuser", user.GetDatabaseUsername())
+// 	})
 
-	t.Run("Setup", func(t *testing.T) {
-		require.NoError(t, user.Setup(ctx))
-		require.True(t, mockCloudResource.isPasswordModified())
-		passwordSet := mockCloudResource.getModifiedPassword()
+// 	t.Run("Setup", func(t *testing.T) {
+// 		require.NoError(t, user.Setup(ctx))
+// 		require.True(t, mockCloudResource.isPasswordModified())
+// 		passwordSet := mockCloudResource.getModifiedPassword()
 
-		// Validate password set for the cloud user is the same one fetched from secrets store.
-		password, err := user.GetPassword(ctx)
-		require.NoError(t, err)
-		require.Equal(t, password, passwordSet)
+// 		// Validate password set for the cloud user is the same one fetched from secrets store.
+// 		password, err := user.GetPassword(ctx)
+// 		require.NoError(t, err)
+// 		require.Equal(t, password, passwordSet)
 
-		// Setup a second time should not fail, and nothing happens.
-		require.NoError(t, user.Setup(ctx))
-		require.False(t, mockCloudResource.isPasswordModified())
-	})
+// 		// Setup a second time should not fail, and nothing happens.
+// 		require.NoError(t, user.Setup(ctx))
+// 		require.False(t, mockCloudResource.isPasswordModified())
+// 	})
 
-	t.Run("RotatePassword not expired", func(t *testing.T) {
-		require.NoError(t, user.RotatePassword(ctx))
-		require.False(t, mockCloudResource.isPasswordModified())
+// 	t.Run("RotatePassword not expired", func(t *testing.T) {
+// 		require.NoError(t, user.RotatePassword(ctx))
+// 		require.False(t, mockCloudResource.isPasswordModified())
 
-		clock.Advance(user.secretTTL / 2)
-		require.NoError(t, user.RotatePassword(ctx))
-		require.False(t, mockCloudResource.isPasswordModified())
-	})
+// 		clock.Advance(user.secretTTL / 2)
+// 		require.NoError(t, user.RotatePassword(ctx))
+// 		require.False(t, mockCloudResource.isPasswordModified())
+// 	})
 
-	t.Run("RotatePassword expired", func(t *testing.T) {
-		clock.Advance(user.secretTTL * 2)
+// 	t.Run("RotatePassword expired", func(t *testing.T) {
+// 		clock.Advance(user.secretTTL * 2)
 
-		require.NoError(t, user.RotatePassword(ctx))
-		require.True(t, mockCloudResource.isPasswordModified())
-		passwordSet := mockCloudResource.getModifiedPassword()
+// 		require.NoError(t, user.RotatePassword(ctx))
+// 		require.True(t, mockCloudResource.isPasswordModified())
+// 		passwordSet := mockCloudResource.getModifiedPassword()
 
-		// Validate password set for the cloud user is the same one saved in secrets store.
-		currentVersion, err := secrets.GetValue(ctx, "local/testuser", libsecrets.CurrentVersion)
-		require.NoError(t, err)
-		require.Equal(t, currentVersion.Value, passwordSet)
+// 		// Validate password set for the cloud user is the same one saved in secrets store.
+// 		currentVersion, err := secrets.GetValue(ctx, "local/testuser", libsecrets.CurrentVersion)
+// 		require.NoError(t, err)
+// 		require.Equal(t, currentVersion.Value, passwordSet)
 
-		// Successfully rotated once, now should use previous version for login.
-		previousVersion, err := secrets.GetValue(ctx, "local/testuser", libsecrets.PreviousVersion)
-		require.NoError(t, err)
+// 		// Successfully rotated once, now should use previous version for login.
+// 		previousVersion, err := secrets.GetValue(ctx, "local/testuser", libsecrets.PreviousVersion)
+// 		require.NoError(t, err)
 
-		password, err := user.GetPassword(ctx)
-		require.NoError(t, err)
-		require.Equal(t, previousVersion.Value, password)
-	})
+// 		password, err := user.GetPassword(ctx)
+// 		require.NoError(t, err)
+// 		require.Equal(t, previousVersion.Value, password)
+// 	})
 
-	t.Run("RotatePassword secret not found", func(t *testing.T) {
-		// Simulate a case that someone else has deleted the secret.
-		require.NoError(t, secrets.Delete(ctx, "local/testuser"))
+// 	t.Run("RotatePassword secret not found", func(t *testing.T) {
+// 		// Simulate a case that someone else has deleted the secret.
+// 		require.NoError(t, secrets.Delete(ctx, "local/testuser"))
 
-		require.NoError(t, user.RotatePassword(ctx))
-		require.True(t, mockCloudResource.isPasswordModified())
-		passwordSet := mockCloudResource.getModifiedPassword()
+// 		require.NoError(t, user.RotatePassword(ctx))
+// 		require.True(t, mockCloudResource.isPasswordModified())
+// 		passwordSet := mockCloudResource.getModifiedPassword()
 
-		password, err := user.GetPassword(ctx)
-		require.NoError(t, err)
-		require.Equal(t, password, passwordSet)
-	})
+// 		password, err := user.GetPassword(ctx)
+// 		require.NoError(t, err)
+// 		require.Equal(t, password, passwordSet)
+// 	})
 
-	t.Run("Teardown", func(t *testing.T) {
-		require.NoError(t, user.Teardown(ctx))
+// 	t.Run("Teardown", func(t *testing.T) {
+// 		require.NoError(t, user.Teardown(ctx))
 
-		_, err := secrets.GetValue(ctx, "local/testuser", libsecrets.CurrentVersion)
-		require.True(t, trace.IsNotFound(err))
-	})
-}
+// 		_, err := secrets.GetValue(ctx, "local/testuser", libsecrets.CurrentVersion)
+// 		require.True(t, trace.IsNotFound(err))
+// 	})
+// }
 
-// mockCloudResource is a mock implementation of cloudResource.
-type mockCloudResource struct {
-	lastPasswordChan chan string
-}
+// // mockCloudResource is a mock implementation of cloudResource.
+// type mockCloudResource struct {
+// 	lastPasswordChan chan string
+// }
 
-func newMockCloudResource() *mockCloudResource {
-	return &mockCloudResource{
-		lastPasswordChan: make(chan string, 1),
-	}
-}
-func (m *mockCloudResource) ModifyUserPassword(ctx context.Context, oldPassword, newPassword string) error {
-	m.lastPasswordChan <- newPassword
-	return nil
-}
-func (m *mockCloudResource) isPasswordModified() bool {
-	return len(m.lastPasswordChan) != 0
-}
-func (m *mockCloudResource) getModifiedPassword() string {
-	if m.isPasswordModified() {
-		return <-m.lastPasswordChan
-	}
-	return ""
-}
+// func newMockCloudResource() *mockCloudResource {
+// 	return &mockCloudResource{
+// 		lastPasswordChan: make(chan string, 1),
+// 	}
+// }
+// func (m *mockCloudResource) ModifyUserPassword(ctx context.Context, oldPassword, newPassword string) error {
+// 	m.lastPasswordChan <- newPassword
+// 	return nil
+// }
+// func (m *mockCloudResource) isPasswordModified() bool {
+// 	return len(m.lastPasswordChan) != 0
+// }
+// func (m *mockCloudResource) getModifiedPassword() string {
+// 	if m.isPasswordModified() {
+// 		return <-m.lastPasswordChan
+// 	}
+// 	return ""
+// }
