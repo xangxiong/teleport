6ea23a5ae25a4374f80abee62ea4807b07dd5905
dl-synesis <dl-synesis-ops@arris.com>
2022-09-30 16:02:32 +0000

    trim auth

--- lib/auth/password.go
+++ lib/auth/password.go
@@ -56,41 +56,6 @@ func (s *Server) ResetPassword(username string) (string, error) {
 	return password, nil
 }
 
-// // checkPasswordWOToken checks just password without checking OTP tokens
-// // used in case of SSH authentication, when token has been validated.
-// func (s *Server) checkPasswordWOToken(user string, password []byte) error {
-// 	const errMsg = "invalid username or password"
-
-// 	err := services.VerifyPassword(password)
-// 	if err != nil {
-// 		return trace.BadParameter(errMsg)
-// 	}
-
-// 	hash, err := s.GetPasswordHash(user)
-// 	if err != nil && !trace.IsNotFound(err) {
-// 		return trace.Wrap(err)
-// 	}
-// 	userFound := true
-// 	if trace.IsNotFound(err) {
-// 		userFound = false
-// 		log.Debugf("Username %q not found, using fake hash to mitigate timing attacks.", user)
-// 		hash = fakePasswordHash
-// 	}
-
-// 	if err = bcrypt.CompareHashAndPassword(hash, password); err != nil {
-// 		log.Debugf("Password for %q does not match", user)
-// 		return trace.BadParameter(errMsg)
-// 	}
-
-// 	// Careful! The bcrypt check above may succeed for an unknown user when the
-// 	// provided password is "barbaz", which is what fakePasswordHash hashes to.
-// 	if !userFound {
-// 		return trace.BadParameter(errMsg)
-// 	}
-
-// 	return nil
-// }
-
 type checkPasswordResult struct {
 	mfaDev *types.MFADevice
 }
