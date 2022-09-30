dd1e5d6897484ff3a2ca23f178828da01aa3f732
dl-synesis <dl-synesis-ops@arris.com>
2022-09-30 23:48:44 +0000

    trim

--- lib/services/identity.go
+++ lib/services/identity.go
@@ -32,23 +32,6 @@ import (
 
 // Identity is responsible for managing user entries and external identities
 type Identity interface {
-	// UpsertWebauthnLocalAuth creates or updates the local auth configuration for
-	// Webauthn.
-	// WebauthnLocalAuth is a component of LocalAuthSecrets.
-	// Automatically indexes the WebAuthn user ID for lookup by
-	// GetTeleportUserByWebauthnID.
-	UpsertWebauthnLocalAuth(ctx context.Context, user string, wla *types.WebauthnLocalAuth) error
-
-	// GetWebauthnLocalAuth retrieves the existing local auth configuration for
-	// Webauthn, if any.
-	// WebauthnLocalAuth is a component of LocalAuthSecrets.
-	GetWebauthnLocalAuth(ctx context.Context, user string) (*types.WebauthnLocalAuth, error)
-
-	// GetTeleportUserByWebauthnID reads a Teleport username from a WebAuthn user
-	// ID (aka user handle).
-	// See UpsertWebauthnLocalAuth and types.WebauthnLocalAuth.
-	GetTeleportUserByWebauthnID(ctx context.Context, webID []byte) (string, error)
-
 	// UpsertWebauthnSessionData creates or updates WebAuthn session data in
 	// storage, for the purpose of later verifying an authentication or
 	// registration challenge.
