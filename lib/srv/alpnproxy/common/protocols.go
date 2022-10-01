770f7473fb93e6debbc3c0743e0723393c484c6d
dl-synesis <dl-synesis-ops@arris.com>
2022-10-01 15:05:28 +0000

    trim protocol

--- lib/srv/alpnproxy/common/protocols.go
+++ lib/srv/alpnproxy/common/protocols.go
@@ -18,32 +18,12 @@ package common
 
 import (
 	"strings"
-
-	"golang.org/x/exp/slices"
 )
 
 // Protocol is the TLS ALPN protocol type.
 type Protocol string
 
 const (
-	// ProtocolPostgres is TLS ALPN protocol value used to indicate Postgres protocol.
-	ProtocolPostgres Protocol = "teleport-postgres"
-
-	// ProtocolMySQL is TLS ALPN protocol value used to indicate MySQL protocol.
-	ProtocolMySQL Protocol = "teleport-mysql"
-
-	// ProtocolMongoDB is TLS ALPN protocol value used to indicate Mongo protocol.
-	ProtocolMongoDB Protocol = "teleport-mongodb"
-
-	// ProtocolRedisDB is TLS ALPN protocol value used to indicate Redis protocol.
-	ProtocolRedisDB Protocol = "teleport-redis"
-
-	// ProtocolSQLServer is the TLS ALPN protocol value used to indicate SQL Server protocol.
-	ProtocolSQLServer Protocol = "teleport-sqlserver"
-
-	// ProtocolSnowflake is TLS ALPN protocol value used to indicate Snowflake protocol.
-	ProtocolSnowflake Protocol = "teleport-snowflake"
-
 	// ProtocolProxySSH is TLS ALPN protocol value used to indicate Proxy SSH protocol.
 	ProtocolProxySSH Protocol = "teleport-proxy-ssh"
 
@@ -73,10 +53,6 @@ const (
 	// traffic intended for the Teleport proxy.
 	ProtocolProxyGRPC Protocol = "teleport-proxy-grpc"
 
-	// ProtocolMySQLWithVerPrefix is TLS ALPN prefix used by tsh to carry
-	// MySQL server version.
-	ProtocolMySQLWithVerPrefix = Protocol(string(ProtocolMySQL) + "-")
-
 	// ProtocolTCP is TLS ALPN protocol value used to indicate plain TCP connection.
 	ProtocolTCP Protocol = "teleport-tcp"
 
@@ -86,17 +62,14 @@ const (
 )
 
 // SupportedProtocols is the list of supported ALPN protocols.
-var SupportedProtocols = append(
-	ProtocolsWithPing(ProtocolsWithPingSupport...),
-	append([]Protocol{
-		ProtocolHTTP2,
-		ProtocolHTTP,
-		ProtocolProxySSH,
-		ProtocolReverseTunnel,
-		ProtocolAuth,
-		ProtocolTCP,
-	}, DatabaseProtocols...)...,
-)
+var SupportedProtocols = []Protocol{
+	ProtocolHTTP2,
+	ProtocolHTTP,
+	ProtocolProxySSH,
+	ProtocolReverseTunnel,
+	ProtocolAuth,
+	ProtocolTCP,
+}
 
 // ProtocolsToString converts the list of Protocols to the list of strings.
 func ProtocolsToString(protocols []Protocol) []string {
@@ -106,39 +79,6 @@ func ProtocolsToString(protocols []Protocol) []string {
 	}
 	return out
 }
-
-// IsDBTLSProtocol returns if DB protocol has supported native TLS protocol.
-// where connection can be TLS terminated on ALPN proxy side.
-// For protocol like MySQL or Postgres where custom TLS implementation is used the incoming
-// connection needs to be forwarded to proxy database service where custom TLS handler is invoked
-// to terminated DB connection.
-func IsDBTLSProtocol(protocol Protocol) bool {
-	dbTLSProtocols := []Protocol{
-		ProtocolMongoDB,
-		ProtocolRedisDB,
-		ProtocolSQLServer,
-		ProtocolSnowflake,
-	}
-
-	return slices.Contains(
-		append(dbTLSProtocols, ProtocolsWithPing(dbTLSProtocols...)...),
-		protocol,
-	)
-}
-
-// DatabaseProtocols is the list of the database protocols supported.
-var DatabaseProtocols = []Protocol{
-	ProtocolPostgres,
-	ProtocolMySQL,
-	ProtocolMongoDB,
-	ProtocolRedisDB,
-	ProtocolSQLServer,
-	ProtocolSnowflake,
-}
-
-// ProtocolsWithPingSupport is the list of protocols that Ping connection is
-// supported. For now, only database protocols are supported.
-var ProtocolsWithPingSupport = DatabaseProtocols
 
 // ProtocolsWithPing receives a list a protocols and returns a list of them with
 // the Ping protocol suffix.
@@ -160,9 +100,4 @@ func ProtocolWithPing(protocol Protocol) Protocol {
 // IsPingProcotol checks if the provided protocol is suffixed with Ping.
 func IsPingProtocol(protocol Protocol) bool {
 	return strings.HasSuffix(string(protocol), string(ProtocolPingSuffix))
-}
-
-// HasPingSupport checks if the provided protocol supports Ping protocol.
-func HasPingSupport(protocol Protocol) bool {
-	return slices.Contains(ProtocolsWithPingSupport, protocol)
 }
