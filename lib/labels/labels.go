66e2ab7f6999bfec4140e33da0b40cf65a26e564
dl-synesis <dl-synesis-ops@arris.com>
2022-10-03 17:05:36 +0000

    trim desktop

--- lib/labels/labels.go
+++ lib/labels/labels.go
@@ -14,8 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package labels provides a way to get dynamic labels. Used by SSH, App,
-// and Kubernetes servers.
+// Package labels provides a way to get dynamic labels.
 package labels
 
 import (
