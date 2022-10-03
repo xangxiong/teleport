fe9d587672b62425caf4c5c1d11dc0d5f3118107
dl-synesis <dl-synesis-ops@arris.com>
2022-10-03 16:23:26 +0000

    trim metrics

--- lib/srv/regular/sshserver.go
+++ lib/srv/regular/sshserver.go
@@ -896,11 +896,11 @@ func (s *Server) PermitUserEnvironment() bool {
 	return s.permitUserEnvironment
 }
 
-func (s *Server) setAdvertiseAddr(addr *utils.NetAddr) {
-	s.Lock()
-	defer s.Unlock()
-	s.advertiseAddr = addr
-}
+// func (s *Server) setAdvertiseAddr(addr *utils.NetAddr) {
+// 	s.Lock()
+// 	defer s.Unlock()
+// 	s.advertiseAddr = addr
+// }
 
 func (s *Server) getAdvertiseAddr() *utils.NetAddr {
 	s.Lock()
@@ -1744,7 +1744,7 @@ func (s *Server) handleAgentForwardNode(req *ssh.Request, ctx *srv.ServerContext
 func (s *Server) handleAgentForwardProxy(req *ssh.Request, ctx *srv.ServerContext) error {
 	// Forwarding an agent to the proxy is only supported when the proxy is in
 	// recording mode.
-	if services.IsRecordAtProxy(ctx.SessionRecordingConfig.GetMode()) == false {
+	if !services.IsRecordAtProxy(ctx.SessionRecordingConfig.GetMode()) {
 		return trace.BadParameter("agent forwarding to proxy only supported in recording mode")
 	}
 
