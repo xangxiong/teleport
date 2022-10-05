183defb21459e331b0d8ed046cba6a23a4474d42
dl-synesis <dl-synesis-ops@arris.com>
2022-10-05 21:25:15 +0000

    trim

--- lib/utils/jsontools.go
+++ lib/utils/jsontools.go
@@ -17,44 +17,9 @@ limitations under the License.
 package utils
 
 import (
-	"bytes"
-	"io"
-	"reflect"
-	"unicode"
-
 	"github.com/gravitational/trace"
 	jsoniter "github.com/json-iterator/go"
-
-	"github.com/ghodss/yaml"
-	kyaml "k8s.io/apimachinery/pkg/util/yaml"
 )
-
-// ToJSON converts a single YAML document into a JSON document
-// or returns an error. If the document appears to be JSON the
-// YAML decoding path is not used (so that error messages are
-// JSON specific).
-// Creds to: k8s.io for the code
-func ToJSON(data []byte) ([]byte, error) {
-	if hasJSONPrefix(data) {
-		return data, nil
-	}
-	return yaml.YAMLToJSON(data)
-}
-
-var jsonPrefix = []byte("{")
-
-// hasJSONPrefix returns true if the provided buffer appears to start with
-// a JSON open brace.
-func hasJSONPrefix(buf []byte) bool {
-	return hasPrefix(buf, jsonPrefix)
-}
-
-// Return true if the first non-whitespace bytes in buf is
-// prefix.
-func hasPrefix(buf []byte, prefix []byte) bool {
-	trim := bytes.TrimLeftFunc(buf, unicode.IsSpace)
-	return bytes.HasPrefix(trim, prefix)
-}
 
 // FastUnmarshal uses the json-iterator library for fast JSON unmarshalling.
 // Note, this function marshals floats with 6 digits precision.
@@ -88,99 +53,4 @@ func FastMarshal(v interface{}) ([]byte, error) {
 	}
 
 	return data, nil
-}
-
-// FastMarshal uses the json-iterator library for fast JSON marshalling
-// with indentation. Note, this function unmarshals floats with 6 digits precision.
-func FastMarshalIndent(v interface{}, prefix, indent string) ([]byte, error) {
-	data, err := SafeConfig.MarshalIndent(v, prefix, indent)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-
-	return data, nil
-}
-
-const yamlDocDelimiter = "---"
-
-// WriteYAML detects whether value is a list
-// and marshals multiple documents delimited by `---`, otherwise, marshals
-// a single value
-func WriteYAML(w io.Writer, values interface{}) error {
-	if reflect.TypeOf(values).Kind() != reflect.Slice {
-		return trace.Wrap(writeYAML(w, values))
-	}
-	// first pass makes sure that all values are documents (objects or maps)
-	slice := reflect.ValueOf(values)
-	allDocs := func() bool {
-		for i := 0; i < slice.Len(); i++ {
-			if !isDoc(slice.Index(i)) {
-				return false
-			}
-		}
-		return true
-	}
-	if !allDocs() {
-		return trace.Wrap(writeYAML(w, values))
-	}
-	// second pass can marshal documents
-	for i := 0; i < slice.Len(); i++ {
-		err := writeYAML(w, slice.Index(i).Interface())
-		if err != nil {
-			return trace.Wrap(err)
-		}
-		if i != slice.Len()-1 {
-			if _, err := w.Write([]byte(yamlDocDelimiter + "\n")); err != nil {
-				return trace.Wrap(err)
-			}
-		}
-	}
-	return nil
-}
-
-// isDoc detects whether value constitutes a document
-func isDoc(val reflect.Value) bool {
-	iterations := 0
-	for val.Kind() == reflect.Interface || val.Kind() == reflect.Ptr {
-		val = val.Elem()
-		// preventing cycles
-		iterations++
-		if iterations > 10 {
-			return false
-		}
-	}
-	return val.Kind() == reflect.Struct || val.Kind() == reflect.Map
-}
-
-// writeYAML writes marshaled YAML to writer
-func writeYAML(w io.Writer, values interface{}) error {
-	data, err := yaml.Marshal(values)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-	_, err = w.Write(data)
-	return trace.Wrap(err)
-}
-
-// ReadYAML can unmarshal a stream of documents, used in tests.
-func ReadYAML(reader io.Reader) (interface{}, error) {
-	decoder := kyaml.NewYAMLOrJSONDecoder(reader, 32*1024)
-	var values []interface{}
-	for {
-		var val interface{}
-		err := decoder.Decode(&val)
-		if err != nil {
-			if err == io.EOF {
-				if len(values) == 0 {
-					return nil, trace.BadParameter("no resources found, empty input?")
-				}
-				if len(values) == 1 {
-					return values[0], nil
-				}
-				return values, nil
-			}
-			return nil, trace.Wrap(err)
-		}
-		values = append(values, val)
-	}
 }
