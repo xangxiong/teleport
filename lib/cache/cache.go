3e7fb859b4c044b0d190a194980e2bbe4ad20670
dl-synesis <dl-synesis-ops@arris.com>
2022-10-05 15:42:54 +0000

    trim

--- lib/cache/cache.go
+++ lib/cache/cache.go
@@ -30,7 +30,6 @@ import (
 	"github.com/gravitational/teleport/lib/services"
 	"github.com/gravitational/teleport/lib/services/local"
 	"github.com/gravitational/teleport/lib/utils"
-	"github.com/gravitational/teleport/lib/utils/interval"
 
 	"github.com/gravitational/trace"
 	"github.com/jonboulle/clockwork"
@@ -411,12 +410,12 @@ func New(config Config) (*Cache, error) {
 		}),
 		closed: atomic.NewBool(false),
 	}
-	collections, err := setupCollections(cs, config.Watches)
-	if err != nil {
-		cs.Close()
-		return nil, trace.Wrap(err)
-	}
-	cs.collections = collections
+	// collections, err := setupCollections(cs, config.Watches)
+	// if err != nil {
+	// 	cs.Close()
+	// 	return nil, trace.Wrap(err)
+	// }
+	// cs.collections = collections
 
 	if config.Unstarted {
 		return cs, nil
@@ -432,20 +431,6 @@ func New(config Config) (*Cache, error) {
 
 // Starts the cache. Should only be called once.
 func (c *Cache) Start() error {
-	retry, err := utils.NewLinear(utils.LinearConfig{
-		First:  utils.HalfJitter(c.MaxRetryPeriod / 10),
-		Step:   c.MaxRetryPeriod / 5,
-		Max:    c.MaxRetryPeriod,
-		Jitter: utils.NewHalfJitter(),
-		Clock:  c.Clock,
-	})
-	if err != nil {
-		c.Close()
-		return trace.Wrap(err)
-	}
-
-	go c.update(c.ctx, retry)
-
 	select {
 	case <-c.initC:
 		if c.initErr == nil {
@@ -482,43 +467,6 @@ Outer:
 	return c.eventsFanout.NewWatcher(ctx, watch)
 }
 
-func (c *Cache) update(ctx context.Context, retry utils.Retry) {
-	defer func() {
-		c.Debugf("Cache is closing, returning from update loop.")
-		// ensure that close operations have been run
-		c.Close()
-	}()
-	timer := time.NewTimer(c.Config.WatcherInitTimeout)
-	for {
-		err := c.fetchAndWatch(ctx, retry, timer)
-		c.setInitError(err)
-		if c.isClosing() {
-			return
-		}
-		if err != nil {
-			c.Warningf("Re-init the cache on error: %v.", err)
-		}
-
-		// events cache should be closed as well
-		c.Debugf("Reloading cache.")
-
-		c.notify(ctx, Event{Type: Reloading, Event: types.Event{
-			Resource: &types.ResourceHeader{
-				Kind: retry.Duration().String(),
-			},
-		}})
-
-		startedWaiting := c.Clock.Now()
-		select {
-		case t := <-retry.After():
-			c.Debugf("Initiating new watch after waiting %v.", t.Sub(startedWaiting))
-			retry.Inc()
-		case <-c.ctx.Done():
-			return
-		}
-	}
-}
-
 func (c *Cache) notify(ctx context.Context, event Event) {
 	if c.EventsC == nil {
 		return
@@ -529,300 +477,6 @@ func (c *Cache) notify(ctx context.Context, event Event) {
 	case <-ctx.Done():
 		return
 	}
-}
-
-// fetchAndWatch keeps cache up to date by replaying
-// events and syncing local cache storage.
-//
-// Here are some thoughts on consistency in face of errors:
-//
-// 1. Every client is connected to the database fan-out
-// system. This system creates a buffered channel for every
-// client and tracks the channel overflow. Thanks to channels every client gets its
-// own unique iterator over the event stream. If client loses connection
-// or fails to keep up with the stream, the server will terminate
-// the channel and client will have to re-initialize.
-//
-// 2. Replays of stale events. Etcd provides a strong
-// mechanism to track the versions of the storage - revisions
-// of every operation that are uniquely numbered and monothonically
-// and consistently ordered thanks to Raft. Unfortunately, DynamoDB
-// does not provide such a mechanism for its event system, so
-// some tradeofs have to be made:
-//
-//	a. We assume that events are ordered in regards to the
-//	individual key operations which is the guarantees both Etcd and DynamodDB
-//	provide.
-//	b. Thanks to the init event sent by the server on a successful connect,
-//	and guarantees 1 and 2a, client assumes that once it connects and receives an event,
-//	it will not miss any events, however it can receive stale events.
-//	Event could be stale, if it relates to a change that happened before
-//	the version read by client from the database, for example,
-//	given the event stream: 1. Update a=1 2. Delete a 3. Put a = 2
-//	Client could have subscribed before event 1 happened,
-//	read the value a=2 and then received events 1 and 2 and 3.
-//	The cache will replay all events 1, 2 and 3 and end up in the correct
-//	state 3. If we had a consistent revision number, we could
-//	have skipped 1 and 2, but in the absence of such mechanism in Dynamo
-//	we assume that this cache will eventually end up in a correct state
-//	potentially lagging behind the state of the database.
-func (c *Cache) fetchAndWatch(ctx context.Context, retry utils.Retry, timer *time.Timer) error {
-	watcher, err := c.Events.NewWatcher(c.ctx, types.Watch{
-		QueueSize: c.QueueSize,
-		Name:      c.Component,
-		Kinds:     c.watchKinds(),
-	})
-	if err != nil {
-		c.notify(c.ctx, Event{Type: WatcherFailed})
-		return trace.Wrap(err)
-	}
-	defer watcher.Close()
-
-	// ensure that the timer is stopped and drained
-	timer.Stop()
-	select {
-	case <-timer.C:
-	default:
-	}
-	// set timer to watcher init timeout
-	timer.Reset(c.Config.WatcherInitTimeout)
-
-	// before fetch, make sure watcher is synced by receiving init event,
-	// to avoid the scenario:
-	// 1. Cache process:   w = NewWatcher()
-	// 2. Cache process:   c.fetch()
-	// 3. Backend process: addItem()
-	// 4. Cache process:   <- w.Events()
-	//
-	// If there is a way that NewWatcher() on line 1 could
-	// return without subscription established first,
-	// Code line 3 could execute and line 4 could miss event,
-	// wrapping up without of sync replica.
-	// To avoid this, before doing fetch,
-	// cache process makes sure the connection is established
-	// by receiving init event first.
-	select {
-	case <-watcher.Done():
-		return trace.ConnectionProblem(watcher.Error(), "watcher is closed: %v", watcher.Error())
-	case <-c.ctx.Done():
-		return trace.ConnectionProblem(c.ctx.Err(), "context is closing")
-	case event := <-watcher.Events():
-		if event.Type != types.OpInit {
-			return trace.BadParameter("expected init event, got %v instead", event.Type)
-		}
-	case <-timer.C:
-		return trace.ConnectionProblem(nil, "timeout waiting for watcher init")
-	}
-	apply, err := c.fetch(ctx)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-
-	// apply will mutate cache, and possibly leave it in an invalid state
-	// if an error occurs, so ensure that cache is not read.
-	c.setReadOK(false)
-	err = apply(ctx)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-
-	// apply was successful; cache is now readable.
-	c.generation.Inc()
-	c.setReadOK(true)
-	c.setInitError(nil)
-
-	// watchers have been queuing up since the last time
-	// the cache was in a healthy state; broadcast OpInit.
-	// It is very important that OpInit is not broadcast until
-	// after we've placed the cache into a readable state.  This ensures
-	// that any derivative caches do not perform their fetch operations
-	// until this cache has finished its apply operations.
-	c.eventsFanout.SetInit()
-	defer c.eventsFanout.Reset()
-
-	retry.Reset()
-
-	// only enable relative node expiry if the cache is configured
-	// to watch for types.KindNode
-	relativeExpiryInterval := interval.NewNoop()
-	for _, watch := range c.Config.Watches {
-		if watch.Kind == types.KindNode {
-			relativeExpiryInterval = interval.New(interval.Config{
-				Duration:      c.Config.RelativeExpiryCheckInterval,
-				FirstDuration: utils.HalfJitter(c.Config.RelativeExpiryCheckInterval),
-				Jitter:        utils.NewSeventhJitter(),
-			})
-			break
-		}
-	}
-	defer relativeExpiryInterval.Stop()
-
-	c.notify(c.ctx, Event{Type: WatcherStarted})
-
-	var lastStalenessWarning time.Time
-	var staleEventCount int
-	for {
-		select {
-		case <-watcher.Done():
-			return trace.ConnectionProblem(watcher.Error(), "watcher is closed: %v", watcher.Error())
-		case <-c.ctx.Done():
-			return trace.ConnectionProblem(c.ctx.Err(), "context is closing")
-		case <-relativeExpiryInterval.Next():
-			if err := c.performRelativeNodeExpiry(ctx); err != nil {
-				return trace.Wrap(err)
-			}
-			c.notify(ctx, Event{Type: RelativeExpiry})
-		case event := <-watcher.Events():
-			// check for expired resources in OpPut events and log them periodically. stale OpPut events
-			// may be an indicator of poor performance, and can lead to confusing and inconsistent state
-			// as the cache may prune items that aught to exist.
-			//
-			// NOTE: The inconsistent state mentioned above is a symptom of a deeper issue with the cache
-			// design.  The cache should not expire individual items.  It should instead rely on OpDelete events
-			// from backend expires. As soon as the cache has expired at least one item, it is no longer
-			// a faithful representation of a real backend state, since it is 'anticipating' a change in
-			// backend state that may or may not have actually happened.  Instead, it aught to serve the
-			// most recent internally-consistent "view" of the backend, and individual consumers should
-			// determine if the resources they are handling are sufficiently fresh.  Resource-level expiry
-			// is a convenience/cleanup feature and aught not be relied upon for meaningful logic anyhow.
-			// If we need to protect against a stale cache, we aught to invalidate the cache in its entirity, rather
-			// than pruning the resources that we think *might* have been removed from the real backend.
-			// TODO(fspmarshall): ^^^
-			//
-			if event.Type == types.OpPut && !event.Resource.Expiry().IsZero() {
-				if now := c.Clock.Now(); now.After(event.Resource.Expiry()) {
-					staleEventCount++
-					if now.After(lastStalenessWarning.Add(time.Minute)) {
-						kind := event.Resource.GetKind()
-						if sk := event.Resource.GetSubKind(); sk != "" {
-							kind = fmt.Sprintf("%s/%s", kind, sk)
-						}
-						c.Warningf("Encountered %d stale event(s), may indicate degraded backend or event system performance. last_kind=%q", staleEventCount, kind)
-						lastStalenessWarning = now
-						staleEventCount = 0
-					}
-				}
-			}
-
-			err = c.processEvent(ctx, event, true)
-			if err != nil {
-				return trace.Wrap(err)
-			}
-			c.notify(c.ctx, Event{Event: event, Type: EventProcessed})
-		}
-	}
-}
-
-// performRelativeNodeExpiry performs a special kind of active expiration where we remove nodes
-// which are clearly stale relative to their more recently heartbeated counterparts as well as
-// the current time. This strategy lets us side-step issues of clock drift or general cache
-// staleness by only removing items which are stale from within the cache's own "frame of
-// reference".
-//
-// to better understand why we use this expiry strategy, it's important to understand the two
-// distinct scenarios that we're trying to accommodate:
-//
-// 1. Expiry events are being emitted very lazily by the real backend (*hours* after the time
-// at which the resource was supposed to expire).
-//
-// 2. The event stream itself is stale (i.e. all events show up late, not just expiry events).
-//
-// In the first scenario, removing items from the cache after they have passed some designated
-// threshold of staleness seems reasonable.  In the second scenario, your best option is to
-// faithfully serve the delayed, but internally consistent, view created by the event stream and
-// not expire any items.
-//
-// Relative expiry is the compromise between the two above scenarios. We calculate a staleness
-// threshold after which items would be removed, but we calculate it relative to the most recent
-// expiry *or* the current time, depending on which is earlier. The result is that nodes are
-// removed only if they are both stale from the perspective of the current clock, *and* stale
-// relative to our current view of the world.
-//
-// *note*: this function is only sane to call when the cache and event stream are healthy, and
-// cannot run concurrently with event processing.
-func (c *Cache) performRelativeNodeExpiry(ctx context.Context) error {
-	// TODO(fspmarshall): Start using dynamic value once it is implemented.
-	gracePeriod := apidefaults.ServerAnnounceTTL
-
-	// latestExp will be the value that we choose to consider the most recent "expired"
-	// timestamp.  This will either end up being the most recently seen node expiry, or
-	// the current time (whichever is earlier).
-	var latestExp time.Time
-
-	nodes, err := c.GetNodes(ctx, apidefaults.Namespace)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-
-	// iterate nodes and determine the most recent expiration value.
-	for _, node := range nodes {
-		if node.Expiry().IsZero() {
-			continue
-		}
-
-		if node.Expiry().After(latestExp) || latestExp.IsZero() {
-			// this node's expiry is more recent than the previously
-			// recorded value.
-			latestExp = node.Expiry()
-		}
-	}
-
-	if latestExp.IsZero() {
-		return nil
-	}
-
-	// if the most recent expiration value is still in the future, we use the current time
-	// as the most recent expiration value instead. Unless the event stream is unhealthy, or
-	// all nodes were recently removed, this should always be true.
-	if now := c.Clock.Now(); latestExp.After(now) {
-		latestExp = now
-	}
-
-	// we subtract gracePeriod from our most recent expiry value to get the retention
-	// threshold. nodes which expired earlier than the retention threshold will be
-	// removed, as we expect well-behaved backends to have emitted an expiry event
-	// within the grace period.
-	retentionThreshold := latestExp.Add(-gracePeriod)
-
-	var removed int
-	for _, node := range nodes {
-		if node.Expiry().IsZero() || node.Expiry().After(retentionThreshold) {
-			continue
-		}
-
-		// remove the node locally without emitting an event, other caches will
-		// eventually remove the same node when they run their expiry logic.
-		if err := c.processEvent(ctx, types.Event{
-			Type: types.OpDelete,
-			Resource: &types.ResourceHeader{
-				Kind:     types.KindNode,
-				Metadata: node.GetMetadata(),
-			},
-		}, false); err != nil {
-			return trace.Wrap(err)
-		}
-
-		// high churn rates can cause purging a very large number of nodes
-		// per interval, limit to a sane number such that we don't overwhelm
-		// things or get too far out of sync with other caches.
-		if removed++; removed >= c.Config.RelativeExpiryLimit {
-			break
-		}
-	}
-
-	if removed > 0 {
-		c.Debugf("Removed %d nodes via relative expiry (retentionThreshold=%s).", removed, retentionThreshold)
-	}
-
-	return nil
-}
-
-func (c *Cache) watchKinds() []types.WatchKind {
-	out := make([]types.WatchKind, 0, len(c.collections))
-	for _, collection := range c.collections {
-		out = append(out, collection.watchKind())
-	}
-	return out
 }
 
 // isClosing checks if the cache has begun closing.
@@ -952,26 +606,6 @@ func (c *Cache) fetch(ctx context.Context) (fn applyFn, err error) {
 		}
 		return nil
 	}, nil
-}
-
-// processEvent hands the event off to the appropriate collection for processing. Any
-// resources which were not registered are ignored. If processing completed successfully
-// and emit is true the event will be emitted via the fanout.
-func (c *Cache) processEvent(ctx context.Context, event types.Event, emit bool) error {
-	resourceKind := resourceKindFromResource(event.Resource)
-	collection, ok := c.collections[resourceKind]
-	if !ok {
-		c.Warningf("Skipping unsupported event %v/%v",
-			event.Resource.GetKind(), event.Resource.GetSubKind())
-		return nil
-	}
-	if err := collection.processEvent(ctx, event); err != nil {
-		return trace.Wrap(err)
-	}
-	if emit {
-		c.eventsFanout.Emit(event)
-	}
-	return nil
 }
 
 type getCertAuthorityCacheKey struct {
