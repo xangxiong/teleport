2dff12c93d338fadd615f5bbbf60ec7f84b23bb4
dl-synesis <dl-synesis-ops@arris.com>
2022-10-05 21:27:43 +0000

    trim

--- lib/utils/repeat.go
+++ lib/utils/repeat.go
@@ -16,40 +16,40 @@ limitations under the License.
 
 package utils
 
-import (
-	"io"
-)
+// import (
+// 	"io"
+// )
 
-// NewRepeatReader returns a repeat reader
-func NewRepeatReader(repeat byte, count int) *RepeatReader {
-	return &RepeatReader{
-		repeat: repeat,
-		count:  count,
-	}
-}
+// // NewRepeatReader returns a repeat reader
+// func NewRepeatReader(repeat byte, count int) *RepeatReader {
+// 	return &RepeatReader{
+// 		repeat: repeat,
+// 		count:  count,
+// 	}
+// }
 
-// RepeatReader repeats the same byte count times
-// without allocating any data, the single instance
-// of the repeat reader is not goroutine safe
-type RepeatReader struct {
-	repeat byte
-	count  int
-	read   int
-}
+// // RepeatReader repeats the same byte count times
+// // without allocating any data, the single instance
+// // of the repeat reader is not goroutine safe
+// type RepeatReader struct {
+// 	repeat byte
+// 	count  int
+// 	read   int
+// }
 
-// Read copies the same byte over and over to the data count times
-func (r *RepeatReader) Read(data []byte) (int, error) {
-	if r.read >= r.count {
-		return 0, io.EOF
-	}
-	var copied int
-	for i := 0; i < len(data); i++ {
-		data[i] = r.repeat
-		copied++
-		r.read++
-		if r.read >= r.count {
-			break
-		}
-	}
-	return copied, nil
-}
+// // Read copies the same byte over and over to the data count times
+// func (r *RepeatReader) Read(data []byte) (int, error) {
+// 	if r.read >= r.count {
+// 		return 0, io.EOF
+// 	}
+// 	var copied int
+// 	for i := 0; i < len(data); i++ {
+// 		data[i] = r.repeat
+// 		copied++
+// 		r.read++
+// 		if r.read >= r.count {
+// 			break
+// 		}
+// 	}
+// 	return copied, nil
+// }
