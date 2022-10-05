1676654bebdc655c591f6e9cc8cb56c9fcb88fcb
dl-synesis <dl-synesis-ops@arris.com>
2022-10-05 21:35:51 +0000

    trim

--- lib/utils/tls.go
+++ lib/utils/tls.go
@@ -17,22 +17,7 @@ limitations under the License.
 package utils
 
 import (
-	"context"
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"math/big"
-	"net"
-	"os"
-	"time"
-
-	"github.com/gravitational/teleport/api/constants"
-	"github.com/gravitational/trace"
-
-	log "github.com/sirupsen/logrus"
 )
 
 // TLSConfig returns default TLS configuration strong defaults.
@@ -55,162 +40,11 @@ func SetupTLSConfig(config *tls.Config, cipherSuites []uint16) {
 	config.ClientSessionCache = tls.NewLRUClientSessionCache(DefaultLRUCapacity)
 }
 
-// CreateTLSConfiguration sets up default TLS configuration
-func CreateTLSConfiguration(certFile, keyFile string, cipherSuites []uint16) (*tls.Config, error) {
-	config := TLSConfig(cipherSuites)
-
-	if _, err := os.Stat(certFile); err != nil {
-		return nil, trace.BadParameter("certificate is not accessible by '%v'", certFile)
-	}
-	if _, err := os.Stat(keyFile); err != nil {
-		return nil, trace.BadParameter("certificate is not accessible by '%v'", certFile)
-	}
-
-	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-	config.Certificates = []tls.Certificate{cert}
-
-	return config, nil
-}
-
-// TLSCredentials keeps the typical 3 components of a proper HTTPS configuration
-type TLSCredentials struct {
-	// PublicKey in PEM format
-	PublicKey []byte
-	// PrivateKey in PEM format
-	PrivateKey []byte
-	Cert       []byte
-}
-
-// macMaxTLSCertValidityPeriod is the maximum validity period
-// for a TLS certificate enforced by macOS.
-// As of Go 1.18, certificates are validated via the system
-// verifier and not in Go.
-const macMaxTLSCertValidityPeriod = 825 * 24 * time.Hour
-
-// GenerateSelfSignedCert generates a self-signed certificate that
-// is valid for given domain names and ips, returns PEM-encoded bytes with key and cert
-func GenerateSelfSignedCert(hostNames []string) (*TLSCredentials, error) {
-	priv, err := rsa.GenerateKey(rand.Reader, constants.RSAKeySize)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-	notBefore := time.Now()
-	notAfter := notBefore.Add(macMaxTLSCertValidityPeriod)
-
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-
-	entity := pkix.Name{
-		CommonName:   "localhost",
-		Country:      []string{"US"},
-		Organization: []string{"localhost"},
-	}
-
-	template := x509.Certificate{
-		SerialNumber:          serialNumber,
-		Issuer:                entity,
-		Subject:               entity,
-		NotBefore:             notBefore,
-		NotAfter:              notAfter,
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-		BasicConstraintsValid: true,
-		IsCA:                  true,
-	}
-
-	// collect IP addresses localhost resolves to and add them to the cert. template:
-	template.DNSNames = append(hostNames, "localhost.local")
-	ips, _ := net.LookupIP("localhost")
-	if ips != nil {
-		template.IPAddresses = append(ips, net.ParseIP("::1"))
-	}
-	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-
-	publicKeyBytes, err := x509.MarshalPKIXPublicKey(priv.Public())
-	if err != nil {
-		log.Error(err)
-		return nil, trace.Wrap(err)
-	}
-
-	return &TLSCredentials{
-		PublicKey:  pem.EncodeToMemory(&pem.Block{Type: "RSA PUBLIC KEY", Bytes: publicKeyBytes}),
-		PrivateKey: pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)}),
-		Cert:       pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes}),
-	}, nil
-}
-
-// CipherSuiteMapping transforms Teleport formatted cipher suites strings
-// into uint16 IDs.
-func CipherSuiteMapping(cipherSuites []string) ([]uint16, error) {
-	out := make([]uint16, 0, len(cipherSuites))
-
-	for _, cs := range cipherSuites {
-		c, ok := cipherSuiteMapping[cs]
-		if !ok {
-			return nil, trace.BadParameter("cipher suite not supported: %v", cs)
-		}
-
-		out = append(out, c)
-	}
-
-	return out, nil
-}
-
-// TLSConn is a `net.Conn` that implements some of the functions defined by the
-// `tls.Conn` struct. This interface can be used where it could receive a
-// `tls.Conn` wrapped in another connection. For example, in the ALPN Proxy,
-// some TLS Connections can be wrapped with ping protocol.
-type TLSConn interface {
-	net.Conn
-
-	// ConnectionState returns basic TLS details about the connection.
-	// More info at: https://pkg.go.dev/crypto/tls#Conn.ConnectionState
-	ConnectionState() tls.ConnectionState
-	// Handshake runs the client or server handshake protocol if it has not yet
-	// been run.
-	// More info at: https://pkg.go.dev/crypto/tls#Conn.Handshake
-	Handshake() error
-	// HandshakeContext runs the client or server handshake protocol if it has
-	// not yet been run.
-	// More info at: https://pkg.go.dev/crypto/tls#Conn.HandshakeContext
-	HandshakeContext(context.Context) error
-}
-
-// cipherSuiteMapping is the mapping between Teleport formatted cipher
-// suites strings and uint16 IDs.
-var cipherSuiteMapping = map[string]uint16{
-	"tls-rsa-with-aes-128-cbc-sha":            tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-	"tls-rsa-with-aes-256-cbc-sha":            tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-	"tls-rsa-with-aes-128-cbc-sha256":         tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
-	"tls-rsa-with-aes-128-gcm-sha256":         tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-	"tls-rsa-with-aes-256-gcm-sha384":         tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-	"tls-ecdhe-ecdsa-with-aes-128-cbc-sha":    tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-	"tls-ecdhe-ecdsa-with-aes-256-cbc-sha":    tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-	"tls-ecdhe-rsa-with-aes-128-cbc-sha":      tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-	"tls-ecdhe-rsa-with-aes-256-cbc-sha":      tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-	"tls-ecdhe-ecdsa-with-aes-128-cbc-sha256": tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-	"tls-ecdhe-rsa-with-aes-128-cbc-sha256":   tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
-	"tls-ecdhe-rsa-with-aes-128-gcm-sha256":   tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-	"tls-ecdhe-ecdsa-with-aes-128-gcm-sha256": tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-	"tls-ecdhe-rsa-with-aes-256-gcm-sha384":   tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-	"tls-ecdhe-ecdsa-with-aes-256-gcm-sha384": tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-	"tls-ecdhe-rsa-with-chacha20-poly1305":    tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-	"tls-ecdhe-ecdsa-with-chacha20-poly1305":  tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-}
-
 const (
 	// DefaultLRUCapacity is a capacity for LRU session cache
 	DefaultLRUCapacity = 1024
-	// DefaultCertTTL sets the TTL of the self-signed certificate (1 year)
-	DefaultCertTTL = (24 * time.Hour) * 365
+	// // DefaultCertTTL sets the TTL of the self-signed certificate (1 year)
+	// DefaultCertTTL = (24 * time.Hour) * 365
 )
 
 // DefaultCipherSuites returns the default list of cipher suites that
